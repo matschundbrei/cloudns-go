b997f3f3a0747dea740734d41b1927aa7cf2d847
Jan Kapellen <jan.kapellen@statravel.com>
2020-01-23 18:33:53 +0100

    adding rough sketches

---
+++ cloudns.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "strings"
+  "net/http"
+  "encoding/json"
+)
+
+const (
+  apiurl = "https://api.cloudns.net"
+)
+
+var (
+  authid int
+  authkey string
+  apitimeout int = 60
+  apiinterval int = 10
+  defaultttl int = 3600
+)
+
+
+func main() {
+  configmap = getConfig()
+}
+
+/* fetch settings from either
+    - configuration file
+    - command line variable
+    - environment variable 
+*/
+func getConfig() (*Apiaccess) {
+  return
+}
+
+func doRequest(url string, rtype string, params map[string]string, auth *Apiaccess) (response, err) {
+  return
+}
+
+func checkrecord(rtype string, rvalue string) (valid bool) {}
+
+func createZone(client{} *ApiClient, domain string) (response{} map[string]string) {}
+func updateZone(client{} *ApiClient, domain string) (response{} map[string]string) {}
+func readZone(client{} *ApiClient, domain string) (response{} map[string]string, zone *ZoneRecord) {}
+func destroyZone(client{} *ApiClient, domain string) (response{} map[string]string) {}
+func createRecord(client{} *ApiClient, domain string, rname string, rtype string, rvalue string) (response{} map[string]string) {}
+func updateRecord(client{} *ApiClient, domain string, rname string, rtype string, rvalue string) (response{} map[string]string) {}
+func readRecord(client{} *ApiClient, domain string, rname string, rtype string, rvalue string) (response{} map[string]string, record *Record) {}
+func destroyRecord(client{} *ApiClient, domain string, rname string, rtype string, rvalue string) (response{} map[string]string) {}
---
+++ types.go
@@ -0,0 +1,41 @@
+package main
+
+// things that you need to access the api
+type Apiaccess struct {
+  authid       *int `json:"auth-id"`
+  authpassword *string `json:"auth-password"`
+}
+
+// things in a records set by api
+type Recordset struct {
+  rtype           *string `json:"record-type"`
+  ttl             int
+  host            string
+  record          string
+  priority        *int `json:",omitempty"`
+  weight          *int `json:",omitempty"`
+  port            *int `json:",omitempty"`
+  frame           *int `json:",omitempty"`
+  frameTitle      *string `json:"frame-title,omitempty"`
+  frameKeywords   *string `json:"frame-keywords,omitempty"`
+  frameDesc       *string `json:"frame-description,omitempty"`
+  savePath        *int `json:"save-path,omitempty"`
+  redirectType    *int `json:"redirect-type,omitempty"`
+  mail            *string `json:",omitempty"`
+  txt             *string `json:",omitempty"`
+  algorithm       *string `json:",omitempty"`
+  fptype          *string `json:",omitempty"`
+  status          *int `json:",omitempty"`
+  geodnsLocation  *int `json:"geodns-location,omitempty"`
+  caaFlag         *int `json:"caa_flag,omitempty"`
+  caaType         *string `json:"caa_type,omitempty"`
+  caaValue        *string `json:"caa_value,omitempty"`
+}
+
+// things in a zone set by api
+type Zone struct {
+  domain *string `json:"domain-name"`
+  ztype  *string `json:"zone-type"`
+  ns     []string `json:,omitempty`
+  master *string `json:"master-ip,omitempty"`
+}
